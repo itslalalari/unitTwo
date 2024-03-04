d1f457b67fb7b601230a4f3f9787480b6e88a528
Larissa <giddings.larissa@hotmail.com>
2024-03-04 00:17:37 -0600

    Final Form

--- closures.js
+++ closures.js
@@ -1,35 +1,36 @@
 function funChar(inputName, inputHP, ...inputMoves){ 
-var outputName = inputName;
-var outputHp = inputHP;
-var outputMoves = inputMoves;
+    var outputName = inputName;
+    var outputHp = inputHP;
+    var outputMoves = inputMoves;
 
-return{
+    return{
         getHP(){
             return outputHp;},
         getName(){
             return outputName;}, 
         getMoves(){
-            var divMoves = document.querySelector (".moves")  //this should select the move div
-            for(const namedMoves of outputMoves ) {
-                p = document.createElement("p"); //for each move create a p
-                const namedMoves = document.createTextNode(outputMoves); //create a textnode for each named moved
-                p.appendChild(divMoves); //append the p for output move
-                //now I have a function that puts the named moves into p
-                
-                
-                //const firstP = document.querySelector("p"); //this needs to be here idk why
-                //const parent = firstP.parentNode;
-
-                //parent.insertBefore(p, firstP.nextElementSibling);
-                //i don't know if these is needed here
+            var divMoves = document.querySelector (".moves");  //this should select the move div
+            for( var moreMoves of outputMoves ) {
+                const para = document.createElement("p"); //for each move create a p
+                para.textContent = "Possible Move: " + moreMoves; 
+                //I don't understand why textContent works but making a textnode doesn't
+                divMoves.append(para);
             }
         }
     }
 }
-    
-var player = funChar("Lari", 9, "hit", "kick", "smack"); // give stats to player
+var divMoves2 = document.querySelector(".moves");    
+var player = funChar("Lari", 9, "Hit", "Kick", "Smack", "Hide", "Scamper Away"); // give stats to player
 
-p = document.createElement("p");
-outputMoves.appendChild("p");
+//use function to pull in char name
+para = document.createElement("p");
+para.textContent = "Player Name: "+ player.getName();
+divMoves2.append(para);
 
+//use function to pull in HP
+para = document.createElement("p");
+para.textContent = "Hit Points: " + player.getHP();
+divMoves2.append(para);
+
+//use function to pull in moves from loop
 player.getMoves();
