3bf3a54c49487ec1beda8d2f1b68cb9e10dd312a
Larissa <giddings.larissa@hotmail.com>
2024-03-17 15:35:27 -0500

    Fixed some readability on .js file

--- async.js
+++ async.js
@@ -1,27 +1,29 @@
 
 async function chase(){ 
- 
-    //create two new variables called runner1 and runner2, both set to false  
+     //create two new variables called runner1 and runner2, both set to false  
 var runner1 = false;
 var runner2 = false; 
     //create a loser variable set to an empty string
 var loser = "";
      //create promise w/resolve and setTimeout, set loser to runner# and resolve as true
 var runner1Go = new Promise(function(resolve, reject){
-    setTimeout(()=>{loser=runner1;resolve(true)}, 2000);    
+    setTimeout(()=> {loser=runner1; resolve(true)}, 2000);    
         });
 var runner2Go = new Promise(function(resolve, reject){
-    setTimeout(()=>{loser=runner2;resolve(true)}, 3000);    
+    setTimeout(()=> {loser=runner2; resolve(true)}, 3000);    
         });
     //Part C = return promises and loser in an array
-return[await runner1Go, await runner2Go, loser];   
-    };
+return[await runner1Go, await runner2Go, loser]
+};
+    /*call async and then clause to run a function; 
+    consol log 'loser' or 3rd position in array.*/ 
 
-//call async and then clause to run a function; 
-    //I don't understand why it needs to be written this way, could param anything?
-    chase().then(function(param){
-        console.log(param[2])});
-//consol log 'loser' or 3rd position in array. loser=false bc loser=runner#=false
+chase().then(function(param){
+    console.log(param[2])});
+    //loser=false bc loser=runner#=false
+
+    /*I don't understand why it needs to be written this way, 
+    could param anything?*/
 
   
 
